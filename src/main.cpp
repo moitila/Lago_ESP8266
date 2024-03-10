c84b16b4adb09f2840410db9bcd03b45eeb3b62a
moitila <trierweiler@gmail.com>
2024-03-10 17:32:36 -0300

    feat(Project) Inicio do projeto

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "MOITILAS";
+const char* password = "CAIPIRINHA";
+
+ESP8266WebServer server(80);
+
+void handleConfigurar() {
+  // Implementar lógica de configuração
+  server.send(200, "application/json", "{\"message\": \"Configuração atualizada\"}");
+}
+
+void handleStatus() {
+  // Implementar lógica de status
+  server.send(200, "application/json", "{\"message\": \"Status enviado\"}");
+}
+
+void handleControlarBomba() {
+  // Implementar controle das bombas
+  server.send(200, "application/json", "{\"message\": \"Comando de bomba enviado\"}");
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Definindo rotas
+  server.on("/", HTTP_GET, []() {
+    server.send(200, "text/plain", "Servidor ESP01 ativo!");
+  });
+
+  server.on("/configurar", HTTP_GET, handleConfigurar);
+  server.on("/status", HTTP_GET, handleStatus);
+  server.on("/controlarBomba", HTTP_GET, handleControlarBomba);
+
+  server.begin();
+  Serial.println("Servidor HTTP iniciado");
+}
+
+void loop() {
+  server.handleClient();
+}
+
